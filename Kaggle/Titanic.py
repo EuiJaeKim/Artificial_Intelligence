22d3f987616f97688aecddfea7cac628211fded9
EuiJaeKim <aktaion@yonsei.ac.kr>
2019-07-21 20:30:27 +0900

    # ANN으로 해보자

--- Kaggle/Titanic.py
+++ Kaggle/Titanic.py
@@ -245,3 +245,28 @@ print(round(np.mean(score)*100, 2))
 
 # submission = pd.read_csv('submission.csv')
 # submission.head()
+
+# -----------------------------------------
+
+# #2. 모델구성
+# from keras.models import Sequential
+# from keras.layers import Dense
+# model = Sequential()
+
+# model.add(Dense(200, input_dim=1, activation='relu'))
+# model.add(Dense(3))
+# model.add(Dense(100))
+# model.add(Dense(4))
+# model.add(Dense(1))
+
+# #3. 훈련
+# model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+# model.fit(x_train, y_train, epochs=30, batch_size=3)
+
+# #4. 평가 예측
+# loss, acc = model.evaluate(x_test, y_test, batch_size=1)
+# print("acc : ", acc)
+
+# # y_predict = model.predict(x_test)
+# y_predict = model.predict(x4)
+# print(y_predict)
