be64516a036f0a9d8b40076177670204fae9f4b9
EuiJaeKim <aktaion@yonsei.ac.kr>
2019-07-30 09:49:08 +0900

    # Max 함수 추가

---
+++ git_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int TC;
+
+    cin >> TC;
+
+    for(int i=0; i < TC;i++){
+        cout << i << " " << endl;
+    }
+    return 0;
+}
+
