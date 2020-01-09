52f546e411daaa0016e8a0e638ad759cffe837c7
EuiJaeKim <aktaion@yonsei.ac.kr>
2020-01-09 18:09:36 +0900

    Upload

--- Test2.py
+++ Test2.py
@@ -12,6 +12,7 @@ from sklearn.metrics import r2_score
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 import matplotlib.pyplot as plt
+from math import *
 
 class LstmModel():
     def __init__(self,InitSplitSize=5):
@@ -19,23 +20,32 @@ class LstmModel():
         if not os.path.exists(self.MODEL_DIR):
             os.mkdir(self.MODEL_DIR)
         self.modelpath="./model/{epoch:02d}-{val_loss:.4f}.hdf5"
-        self.df = pddr.get_data_yahoo('^GSPC', '2000-01-01') # S&P 500 미국 500대
+        self.df = pddr.get_data_yahoo('^GSPC', '1990-01-01') # S&P 500 미국 500대
         # self.LearningRate = 0.005 # 학습률
         self.df = self.df['Close']
         self.df = np.asarray(self.df)
+        self.df = self.df.astype(int)
         self.df = np.reshape(self.df,(-1, 1))
+        self.DataSplit(self.df,SplitSize=InitSplitSize)
+        self.IncDecTrans()
+        self.StandardScalerTrans()
+        self.TrainTestSplit()
+
+        self.Build_Model()
+    def StandardScalerTrans(self):
         scaler = StandardScaler()
         scaler.fit(self.df)
         self._Mean = np.mean(self.df)
         self._Std = np.std(self.df)
-        self.df = scaler.transform(self.df)
-        self.DataSplit(self.df,SplitSize=InitSplitSize)
-        self.TrainTestSplit()
-
-        self.X_train = np.reshape(self.X_train, (len(self.X_train),(InitSplitSize-1), 1))
-        self.X_test = np.reshape(self.X_test, (len(self.X_test),(InitSplitSize-1), 1))
-        self.X_validation = np.reshape(self.X_validation, (len(self.X_validation),(InitSplitSize-1), 1))
-        self.Build_Model()
+        self.X = np.reshape(self.X,(len(self.X),(self.X.shape[1])))
+        self.X = scaler.transform(self.X)
+        self.X = np.reshape(self.X, (len(self.X),(self.X.shape[1]), 1))
+    def IncDecTrans(self):
+        for i, val in enumerate(self.X):
+            if self.Y[i] > self.X[i][val.size-1]:
+                self.Y[i] = 1
+            else:
+                self.Y[i] = 0
     def RMSE(self,y_test,y_predict):
         return np.sqrt(mean_squared_error(y_test,y_predict))
     def DataSplit(self,df,SplitSize=5):
@@ -43,7 +53,6 @@ class LstmModel():
         for i in range(df.size -(SplitSize+1)):# 열
             subset = df[i:(i+SplitSize)]# 0~SplitSize
             dataset.append([item for item in subset])
-        # print(type(dataset))
         self.X = np.array(dataset)[:,0:SplitSize-1]
         self.Y = np.array(dataset)[:,SplitSize-1]
     def TrainTestSplit(self):
@@ -51,16 +60,17 @@ class LstmModel():
         self.X_test,self.X_validation,self.Y_test,self.Y_validation = train_test_split(self.X_test,self.Y_test,test_size=0.5) # 4를 5:5로 나눔
     def Build_Model(self,SplitSize=5):
         self.model = Sequential()
-        self.model.add(LSTM(2048, input_shape=[self.X_train.shape[1],1], return_sequences=True))
-        # self.model.add(LSTM(64, input_shape=[4,1], return_sequences=True))
-        # self.model.add(LSTM(32, return_sequences=True))
+        self.model.add(LSTM(32, input_shape=[self.X_train.shape[1],1], return_sequences=True))
+        # self.model.add(LSTM(8, return_sequences=True))
         self.model.add(LSTM(self.X_train.shape[1]))
-        self.model.add(Dense(1, activation="linear"))
+        # self.model.add(Dense(32,activation='relu'))
+        self.model.add(Dense(8,activation='relu'))
+        self.model.add(Dense(1, activation="sigmoid"))
         self.model.summary()
         self.model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
     def Train(self,InputEpochs=100 ,InputBatch_size=64):
         self.checkpointer = ModelCheckpoint(filepath=self.modelpath, monitor='val_loss', verbose=1, save_best_only=True)
-        self.early_stopping_callback = EarlyStopping(monitor='val_loss', patience=InputEpochs/10) # 트레이닝 하다가 10 이상 좋은 값이 안나오면 중단한다.
+        self.early_stopping_callback = EarlyStopping(monitor='val_loss', patience=InputEpochs/50) # 트레이닝 하다가 10 이상 좋은 값이 안나오면 중단한다.
         self.model_fit = self.model.fit(self.X_train, self.Y_train, epochs=InputEpochs, batch_size=InputBatch_size,
                                         verbose=2, validation_data=(self.X_validation,self.Y_validation),
                                         callbacks=[self.early_stopping_callback,self.checkpointer])
@@ -70,25 +80,28 @@ class LstmModel():
     def Predict(self):
         # 넘파이에서 np.mean 평균값 가져오고 np.std() 표준편차     표준편차를 곱해주고 평균값을 더해준다.
         y_predict = self.model.predict(self.X_test)
-        _Test_y_predict = (y_predict*self._Std) + self._Mean
-        _Test_Y_test = (self.Y_test*self._Std) + self._Mean
-        
         print('y_predict(x_test) : \n', y_predict)
         print("RMSE : ",self.RMSE(self.Y_test, y_predict))
         print("R2 : ", r2_score(self.Y_test, y_predict))
-        print(_Test_y_predict)
-        print(_Test_Y_test)
     def Plt_Chart(self):
         self.df.plot()
         # self.model_fit.plot_predict()
         plt.show()
-
 # 'val_loss' 테스트 셋의 오차
 # 'loss' 학습셋의 오차
 # 'patience' 성능이 증가하지 않는 epoch 을 몇 번이나 허용할 것인가
+def AllRemove():
+    import os
+    file_list = os.listdir('./model/')
+    print(file_list)
+    for i in file_list:
+        if i[0] != '.':
+            sstr = './model/' + i
+            os.remove(sstr)
 
 if __name__ == "__main__":
-    lstm = LstmModel()
-    lstm.Train(InputEpochs=100,InputBatch_size=64)
+    AllRemove()
+    lstm = LstmModel(InitSplitSize=22)
+    lstm.Train(InputEpochs=1000,InputBatch_size=16)
     lstm.Predict()
     # lstm.Plt_Chart()
--- Test3.py
+++ Test3.py
@@ -1,174 +1,55 @@
-# N = 5
-# M = 4
-# visited = [[1] * M] * N
-# visited2 = [[1] * M for _ in range(N)]
-# test = [[1,2,3,4]] + [[5,6,7,8]]
-# test2 = [[1,2,3,4]]
-# print(visited)
-# print(visited2)
+import urllib.request
+from bs4 import BeautifulSoup
+from selenium import webdriver # 웹 애플리케이션의 테스트를 자동화하기 위한 프레임 워크
+from selenium.webdriver.common.keys import Keys
+import time # sleep하기 위한 모듈
+searchsite1 = {'naver':['https://search.naver.com/search.naver?where=image&sm=stb_nmr&', 'nx_query', '_img', 'c:\\data\\naverImages\\'],
+'daum':['http://search.daum.net/search?nil_suggest=btn&w=img&DA=SBC&q=', 'q', 'thumb_img', 'c:\\data\\daumImages\\'],
+'google':['https://www.google.co.kr/imghp?hl=ko&tab=wi','gLFyf','rg_ic rg_i','c:\\data\\googleimages\\'],
+'bing':['http://www.bing.com/?scope=images&FORM=Z9LH1','sb_form_q','mimg','c:\\data\\bingimages\\']}
 
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
+searchsite2 = input('이미지를 검색할 웹브라우저 주소를 입력하세요(naver/daum/google/bing) ')
 
-from pandas import DataFrame
-from pandas_datareader import data, wb
-from pandas_datareader import data as pddr
-from datetime import datetime
+keyword = input('검색어를 입력하세요 : ')
 
-tf.set_random_seed(777)  # reproducibility
-
-def MinMaxScaler(data):
-    numerator = data - np.min(data, 0)
-    denominator = np.max(data, 0) - np.min(data, 0)
-    # noise term prevents the zero division
-    return numerator / (denominator + 1e-7)
-
-# train Parameters
-seq_length = 7
-data_dim = 5
-hidden_dim = 10
-output_dim = 1
-predict_day = 5
-learning_rate = 0.01
-iterations = 500
-LSTM_stack = 1
-
-# Choose stock
-pred_label = 3 # 0 : Open, 1: High 2: Low, 3:Close 4:Volumne
-
-# data scrolling parts
-# 날짜 지정
-now=datetime.now()
-year = now.year
-month = now.month
-day = now.day
-
-start = datetime(2005, 1, 2)
-end = datetime(year, month, day)
-
-df = data.get_data_yahoo('^GSPC', '2004-01-01')
-snp = pddr.get_data_yahoo('^GSPC', '2004-01-01') # S&P 500 미국 500대
-bond2 = pddr.get_data_yahoo('^TYX', '2004-01-01') # Treasury Yield 30 Years, 미국채 30년물
-kospi = pddr.get_data_yahoo('^KS11', '2004-01-01') # Kospi 한국
-bond = pddr.get_data_yahoo('^TNX', '2004-01-01') # Treasury Yield 10 Years, 미국채 10년물
-Nikkei = pddr.get_data_yahoo('^N225', '2004-01-01') # 일본 225대
-
-df.drop(['Adj Close'], axis='columns', inplace=True)
-print(df)
-length=df.shape[0]
-for i in range(1,predict_day+1):
-    df.loc[length+i] = df.iloc[length-1]
-    df.index.values[length+i-1]= datetime(year, month, day+i-1)
-print(df)
-dataset_temp = df.as_matrix()
-
-# Open, High, Low, Volume, Close
-test_min = np.min(dataset_temp, 0)
-test_max = np.max(dataset_temp, 0)
-test_denom = test_max - test_min
-
-dataset = MinMaxScaler(dataset_temp)
-dataset_temp = df.as_matrix()
-test_last_X = (dataset_temp[-seq_length:,:]-test_min)/(test_denom+1e-7)
-
-# build a dataset
-dataX = []
-dataY = []
-dataY_temp = []
-for i in range(0, len(dataset) - seq_length-predict_day+1):
-    _x = dataset[i:i + seq_length]
-    _y = dataset[i + predict_day:i + seq_length+predict_day]
-    #print(i + seq_length+predict_day)
-    dataX.append(_x)
-    dataY.append(_y)
-
-# train/test split 70 / 30
-train_size = int(len(dataY) * 0.7)
-test_size = len(dataY) - train_size
-trainX, testX = np.array(dataX[0:train_size]), np.array(dataX[train_size:len(dataX)])
-trainY, testY = np.array(dataY[0:train_size]), np.array(dataY[train_size:len(dataY)])
-
-# input place holders
-X = tf.placeholder(tf.float32, [None, seq_length, data_dim], name='intput_X')
-Y = tf.placeholder(tf.float32, [None, 1], name='intput_Y')
-
-# build a LSTM network
-def lstm_cell():
-    cell = tf.contrib.rnn.BasicLSTMCell(hidden_dim, state_is_tuple=True) 
-    return cell
-
-multi_cells = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in range(LSTM_stack)], state_is_tuple=True)
-
-outputs_rnn, _states=tf.nn.dynamic_rnn(multi_cells, X, dtype=tf.float32)
-
-X_for_fc = tf.reshape(outputs_rnn, [-1, hidden_dim])
-Y_pred_temp = tf.contrib.layers.fully_connected(X_for_fc, output_dim, activation_fn=None)  
-# reshape out for sequence_loss
-Y_pred = tf.reshape(Y_pred_temp, [-1, seq_length])
-
-# cost/loss
-mean_loss = tf.reduce_sum(tf.square(Y_pred - Y), name='losses_sum')  # sum of the squares
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(mean_loss)
-
-trainY_label=trainY[:,[-1], pred_label]
-testY_label=testY[:,[-1], pred_label]
-
-with tf.Session() as sess:
-    init = tf.global_variables_initializer()
-    sess.run(init)
-
-    # Tensorboard
-    merged = tf.summary.merge_all()
-    writer = tf.summary.FileWriter("./tensorflowlog", sess.graph)
-    
-    losslist = [];
-    # Training step
-    for i in range(iterations):
-    
-        _, step_loss = sess.run([optimizer, mean_loss], feed_dict={X: trainX, Y: trainY_label})
-        losslist = np.append(losslist, step_loss)
-    # Test step
-    test_predict = sess.run(Y_pred, feed_dict={X: testX})
-    # Predictions test
-    prediction_test = sess.run(Y_pred, feed_dict={X: test_last_X.reshape(1, seq_length, data_dim)})
-
-print("predictions ", end='')
-print(prediction_test *test_denom[pred_label]+test_min[pred_label])
-
-
-# Plot losss
-plt.figure(1)
-plt.plot(losslist, color="green", label="Error");
-plt.xlabel("Iteration Number")
-plt.ylabel("Sum of the Squarred Error")
-plt.legend(loc='upper right', frameon=False)
-
-plt.show()
-
-predict_temp=(test_predict*test_denom[pred_label]+test_min[pred_label])
-predict=np.empty((test_predict.shape[0]+seq_length-1 ,seq_length))
-predict[:] = np.NAN
-for i in range(0,seq_length):
-    predict[i:test_predict.shape[0]+i,i]=predict_temp[:,i]
-predict= DataFrame( predict.round())
-
-#predict
-Total_size=df.as_matrix().shape[0]
-Predict_size=predict.shape[0]
-
-predict_mean=predict.mean(axis =1)
-Predict_close_concat=np.concatenate((np.zeros((Total_size-Predict_size,1))[:,-1],predict_mean), axis=0)
-df["Predict Close"]=Predict_close_concat.round()
-
-plt.figure(3)
-df.iloc[Total_size-Predict_size:,3].plot(label="Real")
-df.iloc[Total_size-Predict_size:,5].plot(label="Prediction")
-plt.legend(loc='upper left', frameon=False)
-plt.show()
-
-plt.figure(3)
-df.iloc[-100:,3].plot(label="Real")
-df.iloc[-100:,5].plot(label="Prediction")
-plt.legend(loc='upper left', frameon=False)
-plt.show()
+# searchsite1['{}'.format(searchsite2)][2]
+###########################url 받아오기###########################
+# 크롬드라이버 경로설정(사전에 설치필요)
+# 팬텀JS를 사용하면 백그라운드로 실행할 수 있다.
+chrome = '.\chromedriver.exe'
+browser = webdriver.Chrome(chrome) # 웹브라우저 인스턴스화
+browser.get(searchsite1['{}'.format(searchsite2)][0])
+# 이미지를 검색할 웹사이트의 주소 입력(이미지만 검색하는 창을 추천한다.)
+elem = browser.find_element_by_id(searchsite1['{}'.format(searchsite2)][1]) #naver.com같은 경우에는 "nx_query"
+elem = browser.find_element_by_class_name("gLFyf")
+###########################검색어 입력#############################
+elem.send_keys(keyword) # 검색어 입력(검색어 입력창과 연결)
+elem.submit() # Enter키
+###########################반복 횟수##############################
+for i in range(1, 2):
+    browser.find_element_by_xpath("//body").send_keys(Keys.END)
+#Enter키를 누르면 body를 활성화하겠다.(마우스로 클릭하는 개념)
+time.sleep(5)
+time.sleep(7) # 네트워크의 상태를 고려하여 sleep
+html = browser.page_source # 크롬 브라우저에서 현재 불러온 소스를 가져온다.
+soup = BeautifulSoup(html, "lxml") # html코드를 검색할 수 있도록 설정
+############################그림파일 저장##############################
+def fetch_list_url():
+    params = []
+    imgList = soup.find_all("img", class_=searchsite1['{}'.format(searchsite2)][2]) # 네이버 이미지 url이 있는 태그의 _img클래스에 가서
+    for img in imgList:
+        try:
+            params.append(img["src"])
+        except KeyError:
+            pass
+    return params
+def fetch_detail_url():
+    params = fetch_list_url()
+    for idx, img in enumerate(params, 1):
+        urllib.request.urlretrieve(img, searchsite1['{}'.format(searchsite2)][3] + str(idx) + ".jpg")
+    # 다운로드 받을 경로 입력
+if __name__ == '__main__':
+    # 메인실행 함수
+    fetch_detail_url()
+    #끝나면 브라우저 닫기
+    browser.quit()
